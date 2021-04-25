8526475eaf5ea0bd43335caf80949ad7f103434e
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-25 23:35:11 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+//Array of objects
+const quiz = [
+    {
+        q:"You're ___ hour late.",
+        options:["an", "a", "some"],
+        answer: 0
+    },
+    {
+     q:"I love ___ music.",
+        options:["a", "-", "the"],
+        answer: 1   
+    },
+    {
+        q:"My father's mother is my ___ .",
+        options:["niece", "aunt", "grandmother"],
+        answer: 2
+    },
+    {
+        q:"This is my brother. ___ name's Liam.",
+        options:["His", "Him", "He"],
+        answer: 0
+    },
+    {
+        q:"This is wrong. ___ Paul. My name's Eric.",
+        options:["I'm", "I'm not", "You are"],
+        answer: 1
+    },
+    {
+        q:"You take a shower in the ___ .",
+        options:["dining-room", "bedroom", "bathroom"],
+        answer: 2
+    },
+    {
+        q:"I ___ a book every day.",
+        options:["read", "red", "ride"],
+        answer: 0
+    },
+    {
+        q:"It was ___ great day. ",
+        options:["an", "a", "the"],
+        answer: 1
+    },
+    {
+        q:"Your bag is ___ the table.",
+        options:["between", "next", "on"],
+        answer: 2
+    },
+    {
+        q:"We haven't got ___ time.",
+        options:["much", "many", "some"],
+        answer: 0
+    },
+    {
+        q:"'John, wash ___ hands!'",
+        options:["you", "your", "yours"],
+        answer: 1
+    },
+    {
+        q:"'Oh, ___ shoes are killing me!'",
+        options:["this", "that", "these"],
+        answer: 2
+    },
+    {
+        q:"The Browns have got six ___ .",
+        options:["children", "childrens", "child"],
+        answer: 0
+    },
+    {
+        q:"I go to school ___ bus.",
+        options:["in", "by", "on"],
+        answer: 1
+    },
+    {
+        q:"He ___ 10 years old.",
+        options:["has got", "has", "is"],
+        answer: 2
+    },
+    {
+        q:"Claire ___ with my cousin.",
+        options:["works", "work", "workes"],
+        answer: 0
+    },
+    {
+        q:"There are ___ shops in this town.",
+        options:["much", "many", "any"],
+        answer: 1
+    },
+    {
+        q:"Laura and I are friends. ___ are best friends.",
+        options:["You", "They", "We"],
+        answer: 2
+    },
+    {
+        q:"It's my ___ .",
+        options:["mother's bag", "mother bag", "mothers' bag"],
+        answer: 0
+    },
+    {
+        q:"Where ___ ?",
+        options:["they live", "do they live", "are they live"],
+        answer: 1
+    }
+]
